9cc88cc6f258a802edbadecb7428317fc7a5b1a3
Abhijeet Kumar <abkumar4@q3infotech.net>
2023-04-14 22:23:26 -0700

    added mongodb atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
+require('dotenv').config();
+const DBLINK = process.env.DBLINK
 
-const DBLINK = "mongodb://127.0.0.1:27017/Ecommerce2"
+// const DBLINK = "mongodb://127.0.0.1:27017/Ecommerce2"
+// const DBLINK = "mongodb+srv://abhijeet:abhijeet9242@cluster0.jeqho.mongodb.net/Ecommerce2"
 
 module.exports = () =>{
     return mongoose.connect(DBLINK)
